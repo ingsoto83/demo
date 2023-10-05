545b16acd2dda6828632da041775e2069ab9db7d
alejandro.soto <soto.its@gmail.com>
2023-10-04 19:14:02 -0600

    4 Oct

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:demo/fullscreen_widget.dart';
+import 'package:demo/travel_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
       title: "DemoApp",
-      home: FullscreenWidget()
+      home: TravelWidget()
     )
   );
 }
